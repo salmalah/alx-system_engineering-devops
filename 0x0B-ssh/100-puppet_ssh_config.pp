68f4653547c5e3d53e7f85a22ab8c213144fa4cb
salmalah <s.salmalahmidi@gmail.com>
2023-08-26 12:00:31 -0700

    test

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,2 +1,14 @@
 #  SSH client configuration 
+file_line { 'Turn off passwd auth':
+  path => '/etc/ssh/ssh_config',
+  line => 'PasswordAuthentication no',
+}
 
+file_line { 'Declare identity file':
+  path => '/etc/ssh/ssh_config',
+  line => 'IdentityFile ~/.ssh/school',
+}
+service { 'ssh':
+  ensure => running,
+  enable => true,
+}
