bcc74d2cbd374b116f2ef507af5154fe4c4d51c1
salmalah <122815714+salmalah@users.noreply.github.com>
2023-10-23 19:11:33 +0100

    Update 0-gather_data_from_an_API.py

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -5,22 +5,35 @@ a Python script that, using this REST API, for a given employee ID
 import requests
 import sys
 
-
 if __name__ == "__main__":
-    employee_id = sys.argv[1]
-    url = "https://jsonplaceholder.typicode.com"
-    user_response = requests.get("{}/users/{}".format(url, employee_id))
-    todos_response = requests.get("{}/todos?userId={}"
-                                  .format(url, employee_id))
+    if len(sys.argv) != 2:
+        print("Usage: {} <employee_id>".format(sys.argv[0]))
+        sys.exit(1)
+    emp_id = sys.argv[1]
+    user_url = 'https://jsonplaceholder.typicode.com/users/{}'.format(emp_id)
+    user_response = requests.get(user_url)
 
-    employee_name = user_response.json().get('name')
-    todos = todos_response.json()
+    if user_response.status_code != 200:
+        print("Error: Unable to fetch user data from the API")
+        sys.exit(1)
 
+    user_data = user_response.json()
+    employee_name = user_data.get('name', 'Unknown')
+    tasks_url = 'https://jsonplaceholder.typicode.com/todos'
+    tasks_params = {'userId': emp_id}
+    tasks_response = requests.get(tasks_url, params=tasks_params)
+
+    if tasks_response.status_code != 200:
+        print("Error: Unable to fetch tasks data from the API")
+        sys.exit(1)
+
+    todos = tasks_response.json()
+
+    completed_tasks = [todo for todo in todos if todo['completed']]
+    num_completed_tasks = len(completed_tasks)
     total_tasks = len(todos)
-    done_tasks = [todo for todo in todos if todo["completed"]]
-    num_done_tasks = len(done_tasks)
 
-    print("Employee {} is done with tasks(\
-{}/{}):".format(employee_name, num_done_tasks, total_tasks))
-    for task in done_tasks:
+    print("Employee {} is done with tasks({}/{}):".format(
+        employee_name, num_completed_tasks, total_tasks))
+    for task in completed_tasks:
         print("\t {}".format(task['title']))
