34899d9d057b5670b3f741d981ae16a098a93c70
salmalah <s.salmalahmidi@gmail.com>
2023-10-23 10:37:12 -0700

    ok

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -9,9 +9,8 @@ if __name__ == "__main__":
     if len(sys.argv) != 2:
         print("Usage: {} <employee_id>".format(sys.argv[0]))
         sys.exit(1)
-
-    employee_id = sys.argv[1]
-    user_url = 'https://jsonplaceholder.typicode.com/users/{}'.format(employee_id)
+    emp_id = sys.argv[1]
+    user_url = 'https://jsonplaceholder.typicode.com/users/{}'.format(emp_id)
     user_response = requests.get(user_url)
 
     if user_response.status_code != 200:
@@ -21,7 +20,7 @@ if __name__ == "__main__":
     user_data = user_response.json()
     employee_name = user_data.get('name', 'Unknown')
     tasks_url = 'https://jsonplaceholder.typicode.com/todos'
-    tasks_params = {'userId': employee_id}
+    tasks_params = {'userId': emp_id}
     tasks_response = requests.get(tasks_url, params=tasks_params)
 
     if tasks_response.status_code != 200:
@@ -38,4 +37,3 @@ if __name__ == "__main__":
         employee_name, num_completed_tasks, total_tasks))
     for task in completed_tasks:
         print("\t {}".format(task['title']))
-
