debe66749d2c693290f45e538c32533e4b34af81
salmalah <122815714+salmalah@users.noreply.github.com>
2023-11-07 18:25:34 +0100

    Update 2-recurse.py

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -7,27 +7,25 @@ import requests
 def recurse(subreddit, hot_list=[], after=None):
     if subreddit is None or not isinstance(subreddit, str):
         return None
-    if hot_list is None:
-        hot_list = []
     apiUrl = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
-    params = {'limit': 100, 'after': after}
+    params = {"after": after}
     headers = {"User-Agent": "MyCoolReqName/1.0 (by /u/ReplyAdventurous5909)"}
+    response = requests.get(apiUrl, headers=headers,
+                            params=params, allow_redirects=False)
+    if response.status_code == 200:
+        data = response.json()
+        children = data["data"]["children"]
 
-    resp = requests.get(apiUrl, params=params, headers=headers)
-    if resp.status_code == 200:
-        data = resp.json()
-        posts = data.get('data', {}).get('children', [])
-        
-        for post in posts:
-            title = post['data'].get('title')
-            if title:
-                hot_list.append(title)
-        
-        after = data.get('data', {}).get('after')
+        for child in children:
+            hot_list.append(child["data"]["title"])
+
+        after = data["data"]["after"]
         if after:
-            recurse(subreddit, hot_list, after)
-        
-        return hot_list
+            return recurse(subreddit, hot_list, after)
+        else:
+            return hot_list
+    elif response.status_code == 404:
+        return None
     else:
         return None
 
