d9c481b98461755094efd2f6a80be899577708ae
salmalah <s.salmalahmidi@gmail.com>
2023-11-07 10:37:38 -0800

    ok

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -3,14 +3,10 @@
 100-count
 """
 import requests
-from collections import defaultdict
 
-def count_words(subreddit, word_list, after=None, word_count=None):
+def count_words(subreddit, word_list, after=None, word_count={}):
     if subreddit is None or not isinstance(subreddit, str):
         return
-
-    if word_count is None:
-        word_count = defaultdict(int)
 
     apiUrl = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
     params = {'limit': 100, 'after': after}
@@ -24,19 +20,15 @@ def count_words(subreddit, word_list, after=None, word_count=None):
         for post in posts:
             title = post['data'].get('title', '').lower()
             for word in word_list:
-                word_lower = word.lower()
-                if word_lower in title and not title.startswith(word_lower + '.') \
-                        and not title.startswith(word_lower + '!') \
-                        and not title.startswith(word_lower + '_'):
-                    word_count[word_lower] += title.count(word_lower)
+                if word.lower() in title and not title.startswith(word.lower() + '.') and not title.startswith(word.lower() + '!') and not title.startswith(word.lower() + '_'):
+                    word_count[word] = word_count.get(word, 0) + title.count(word.lower())
 
         after = data.get('data', {}).get('after')
         if after:
             count_words(subreddit, word_list, after, word_count)
         else:
-            sorted_word_count = sorted(word_count.items(), key=lambda x: (-x[1], x[0]))
+            sorted_word_count = sorted(word_count.items(), key=lambda x: (-x[1], x[0].lower()))
             for word, count in sorted_word_count:
                 print("{}: {}".format(word.lower(), count))
     else:
         return
-
