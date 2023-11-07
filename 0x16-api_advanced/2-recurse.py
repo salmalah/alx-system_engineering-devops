c0d25494cf30dec9f6e997bc3b1cfcf1a4ff00ae
salmalah <122815714+salmalah@users.noreply.github.com>
2023-11-07 18:28:54 +0100

    Update 2-recurse.py

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -4,28 +4,27 @@ Recursivly getting hot topics
 """
 import requests
 
+
 def recurse(subreddit, hot_list=[], after=None):
     if subreddit is None or not isinstance(subreddit, str):
         return None
     apiUrl = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
     params = {"after": after}
     headers = {"User-Agent": "MyCoolReqName/1.0 (by /u/ReplyAdventurous5909)"}
-    response = requests.get(apiUrl, headers=headers,
-                            params=params, allow_redirects=False)
-    if response.status_code == 200:
-        data = response.json()
-        children = data["data"]["children"]
-
-        for child in children:
-            hot_list.append(child["data"]["title"])
-
-        after = data["data"]["after"]
+    resp = requests.get(apiUrl, params=params, headers=headers)
+    if resp.status_code == 200:
+        data = resp.json()
+        posts = data.get('data', {}).get('children', [])
+        
+        for post in posts:
+            title = post['data'].get('title')
+            if title:
+                hot_list.append(title)
+        
+        after = data.get('data', {}).get('after')
         if after:
-            return recurse(subreddit, hot_list, after)
-        else:
-            return hot_list
-    elif response.status_code == 404:
-        return None
+            recurse(subreddit, hot_list, after)
+        
+        return hot_list
     else:
         return None
-
