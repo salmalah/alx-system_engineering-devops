c846123cf9ffa37ae913cd1b9edf2cb76e3ba414
salmalah <s.salmalahmidi@gmail.com>
2023-11-16 12:21:24 +0000

    su - holberton

---
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -0,0 +1,11 @@
+# File: 1-user_limit.pp
+# Description: Puppet manifest to change user limits
+exec { 'increase-hard-file-limit':
+  command => 'sed -i "/holberton hard/s/5/50000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/'
+}
+
+exec { 'increase-soft-file-limit':
+  command => 'sed -i "/holberton soft/s/4/50000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/'
+}
